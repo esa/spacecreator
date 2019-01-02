5098f3c9fad293637ef6a2b4e0a05fc54c730ca4
Günter Schwann <guenter@vikingsoftware.com>
2019-01-02 17:58:57 +0100

    Linux: generated the parser only if needed (faster re-compile)

--- scripts/create_msc_parser.sh
+++ scripts/create_msc_parser.sh
@@ -7,4 +7,14 @@ alias antlr4='java -jar $ANTLR_JAR'
 
 cd libs/msclibrary/parser
 
-antlr4 -Dlanguage=Cpp -no-listener -visitor Msc.g4
+# Was the file generated already?
+if [ ! -f MscParser.h ]; then
+    echo "Need to build the parser files for the first time"
+    antlr4 -Dlanguage=Cpp -no-listener -visitor Msc.g4
+fi
+
+# If the grammar is newer than the generated file
+if [ Msc.g4 -nt MscParser.h ]; then
+    echo "Need to update build the parser files"
+    antlr4 -Dlanguage=Cpp -no-listener -visitor Msc.g4
+fi
