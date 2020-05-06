79ec2dd278b399ecd31c288ec25ec3f4c96d6db5
Guenter Schwann <guenter@vikingsoftware.com>
2020-05-06 15:46:31 +0200

    7 - Set a name for the registered metatypes.

--- src/libs/msclibrary/msclibrary.cpp
+++ src/libs/msclibrary/msclibrary.cpp
@@ -23,15 +23,15 @@ void initMscLibrary()
 {
     init_msc_library();
 
-    qRegisterMetaType<QList<msc::MscMessageDeclaration *>>();
-    qRegisterMetaType<QVector<msc::MscChart *>>();
-    qRegisterMetaType<QVector<msc::MscComment *>>();
-    qRegisterMetaType<QVector<msc::MscDocument *>>();
-    qRegisterMetaType<QVector<msc::MscEntity *>>();
-    qRegisterMetaType<QVector<msc::MscInstance *>>();
+    qRegisterMetaType<QList<msc::MscMessageDeclaration *>>("QList<msc::MscMessageDeclaration*>");
+    qRegisterMetaType<QVector<msc::MscChart *>>("QVector<msc::MscChart*>");
+    qRegisterMetaType<QVector<msc::MscComment *>>("QVector<msc::MscComment*>");
+    qRegisterMetaType<QVector<msc::MscDocument *>>("QVector<msc::MscDocument*>");
+    qRegisterMetaType<QVector<msc::MscEntity *>>("QVector<msc::MscEntity*>");
+    qRegisterMetaType<QVector<msc::MscInstance *>>("QVector<msc::MscInstance*>");
     qRegisterMetaType<msc::MscDocument::HierarchyType>("HierarchyType");
-    qRegisterMetaType<msc::MscInstanceEvent *>();
-    qRegisterMetaType<msc::MscMessageDeclaration *>();
+    qRegisterMetaType<msc::MscInstanceEvent *>("msc::MscInstanceEvent*");
+    qRegisterMetaType<msc::MscMessageDeclaration *>("msc::MscMessageDeclaration*");
 }
 
 }
