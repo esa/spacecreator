00c3878fbafb4d2c218435b67b0884935c847ee8
Guenter Schwann <guenter@vikingsoftware.com>
2020-06-22 13:18:46 +0200

    69: Move msceditor and cleanup scripts

--- src/applications/msceditor/scripts/clang_format_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-find libs/ -iname *.cpp -o -iname *.h | xargs clang-format -style=file -i
-find msceditor/ -iname *.cpp -o -iname *.h | xargs clang-format -style=file -i
-find tests/ -iname *.cpp -o -iname *.h | xargs clang-format -style=file -i
