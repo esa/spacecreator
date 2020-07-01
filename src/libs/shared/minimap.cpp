3798288abccba8e1ab46e705dc16c50c43694fa7
Denis Gofman <denis@vikingsoftware.com>
2020-07-01 18:52:32 +0300

    MiniMap now is QWidget (not a QDockWidget)

--- src/libs/shared/minimap.cpp
+++ src/libs/shared/minimap.cpp
@@ -20,23 +20,51 @@
 #include <QDebug>
 #include <QGraphicsScene>
 #include <QGraphicsView>
+#include <QPainter>
 #include <QPointer>
+#include <QTimer>
+
+#define LOG qDebug() << Q_FUNC_INFO
 
 namespace shared {
 namespace ui {
 
 struct MiniMapPrivate {
+    MiniMapPrivate()
+        : m_renderTimer(new QTimer)
+    {
+        static constexpr int renderDelayMs = 500;
+        m_renderTimer->setInterval(renderDelayMs);
+        m_renderTimer->setSingleShot(true);
+    }
+
+    ~MiniMapPrivate()
+    {
+        delete m_renderTimer;
+        m_renderTimer = nullptr;
+    }
+
     bool isReady() const { return m_view && m_scene; }
 
     QPointer<QGraphicsView> m_view { nullptr };
     QPointer<QGraphicsScene> m_scene { nullptr };
+    QTimer *m_renderTimer { nullptr };
+
+    QPixmap m_sceneContent;
+    QRect m_sceneViewport;
+
+    QPixmap m_display;
 };
 
 MiniMap::MiniMap(QWidget *parent)
-    : QDockWidget(parent)
+    : QWidget(parent, Qt::Tool)
     , d(new MiniMapPrivate)
 {
     qDebug() << Q_FUNC_INFO << parent;
+    connect(d->m_renderTimer, &QTimer::timeout, this, &MiniMap::updateSceneContent);
+
+    static constexpr int minDimensionPix = 100;
+    setMinimumSize(minDimensionPix, minDimensionPix);
 }
 
 MiniMap::~MiniMap() {}
@@ -44,26 +72,121 @@ MiniMap::~MiniMap() {}
 void MiniMap::setupSourceView(QGraphicsView *view)
 {
     if (d->m_view) {
-        // disconnect
+        if (d->m_scene) {
+            disconnect(d->m_scene, &QGraphicsScene::changed, this, &MiniMap::onSceneUpdated);
+            d->m_scene = nullptr;
+        }
+
+        //        disconnect(d->m_view);
+        d->m_view = nullptr;
     }
 
     d->m_view = view;
 
     if (d->m_view) {
+        //        connect(d->m_view,&QGraphicsView)
+        if (auto *scene = d->m_view->scene()) {
+            d->m_scene = scene;
+            connect(d->m_scene, &QGraphicsScene::changed, this, &MiniMap::onSceneUpdated);
+        }
         // connect
     }
 }
 
 void MiniMap::showEvent(QShowEvent *e)
 {
+    LOG << 1 << isVisible();
     QWidget::showEvent(e);
-    Q_EMIT visibilityChanged(isVisible());
+    const bool visible = isVisible();
+    Q_EMIT visibilityChanged(visible);
+
+    if (visible) {
+        composeMap();
+    }
+    LOG << 2 << isVisible();
 }
 
 void MiniMap::hideEvent(QHideEvent *e)
 {
+    LOG << 1 << isVisible();
+
     QWidget::hideEvent(e);
     Q_EMIT visibilityChanged(isVisible());
+
+    LOG << 2 << isVisible();
+}
+
+void MiniMap::paintEvent(QPaintEvent *event)
+{
+    QWidget::paintEvent(event);
+
+    if (!d->m_display.isNull()) {
+        QPainter painter(this);
+        painter.drawPixmap(rect(), d->m_display);
+    }
+}
+
+void MiniMap::resizeEvent(QResizeEvent *e)
+{
+    QWidget::resizeEvent(e);
+
+    composeMap();
+}
+
+void MiniMap::onSceneUpdated()
+{
+    LOG;
+    // Depending on the actual scene content/size,
+    // the rendering process might be a resources consuming task.
+    // Do it only when really necessary.
+    if (!isVisible()) {
+        return;
+    }
+
+    if (d->m_renderTimer->isActive())
+        d->m_renderTimer->stop();
+    d->m_renderTimer->start();
+}
+
+void MiniMap::onViewUpdated()
+{
+    LOG;
+    // Just to keep it consistent with the scene's content
+    if (!isVisible()) {
+        return;
+    }
+
+    updateViewportFrame();
+}
+
+void MiniMap::updateSceneContent()
+{
+    LOG;
+    d->m_sceneContent = QPixmap(d->m_scene->sceneRect().size().toSize());
+    d->m_sceneContent.fill(Qt::transparent);
+    QPainter p(&d->m_sceneContent);
+    d->m_scene->render(&p);
+
+    composeMap();
+}
+
+void MiniMap::updateViewportFrame()
+{
+    LOG;
+    composeMap();
+}
+
+void MiniMap::composeMap()
+{
+    LOG;
+    const QPixmap scaledContent = d->m_sceneContent.scaled(size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+    d->m_display = QPixmap(size());
+    d->m_display.fill(Qt::white);
+
+    QPainter painter(&d->m_display);
+    painter.drawPixmap(d->m_display.rect(), scaledContent, scaledContent.rect());
+    update();
 }
 
 } // namespace ui
