ba51201d70be93b63c7b22aab9bc6c29e93d8210
Guenter Schwann <guenter@vikingsoftware.com>
2020-09-22 13:34:49 +0200

    Use red as error color in MSC editor

--- src/libs/libmsceditor/baseitems/common/mscutils.h
+++ src/libs/libmsceditor/baseitems/common/mscutils.h
@@ -32,7 +32,7 @@ class InstanceItem;
 
 constexpr qreal LineHoverTolerance = 10.;
 constexpr qreal LineHorizontalityTolerance = 15.;
-const QColor AadlErrorColor = { 255, 128, 0 };
+const QColor AadlErrorColor = QColor(Qt::red);
 const QColor MessageColor = { "#3e47e6" };
 
 enum class CifUpdatePolicy
