20fe4216e7c4af6d28e2ff7bd453950bbb961169
Guenter Schwann <guenter@vikingsoftware.com>
2020-11-18 10:50:12 +0100

    273 Fix clearing the .asn file in msc when using message declarations

--- src/libs/libmsceditor/msceditorcore.cpp
+++ src/libs/libmsceditor/msceditorcore.cpp
@@ -741,6 +741,7 @@ void MSCEditorCore::openMessageDeclarationEditor(QWidget *parentwidget)
 
     MessageDeclarationsDialog dialog(
             docs.at(0)->messageDeclarations(), model, commandsStack(), m_aadlChecks.get(), parentwidget);
+    dialog.setFileName(model->dataDefinitionString());
     dialog.setAadlConnectionNames(m_aadlChecks->connectionNames());
     int result = dialog.exec();
     if (result == QDialog::Accepted) {
