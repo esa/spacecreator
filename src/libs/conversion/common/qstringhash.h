3818700157c70bcd3d64b0b1724d52aa5863caea
Filip Demski <fdemski@n7space.com>
2021-08-27 11:16:30 +0200

    Conversion: Added QT version check around hash implementation for QString

--- src/libs/conversion/common/qstringhash.h
+++ src/libs/conversion/common/qstringhash.h
@@ -23,13 +23,15 @@
 #include <QString>
 #include <functional>
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
 namespace std {
 
 template<>
 struct hash<QString> {
-    std::size_t operator()(const QString& s) const noexcept {
+    std::size_t operator()(const QString &s) const noexcept {
       return (size_t) qHash(s);
     }
 };
 
-}
+} // namespace std
+#endif
