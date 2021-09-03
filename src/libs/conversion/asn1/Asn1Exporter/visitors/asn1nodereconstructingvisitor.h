319b3cfcbf8e246eb74d40b57dff89030a867b15
Mikolaj Owczarzak <mowczarzak@n7space.com>
2021-09-03 17:49:12 +0200

    Asn1AcnExporter: updated the license text in backported code

--- src/libs/conversion/asn1/Asn1Exporter/visitors/asn1nodereconstructingvisitor.h
+++ src/libs/conversion/asn1/Asn1Exporter/visitors/asn1nodereconstructingvisitor.h
@@ -1,28 +1,22 @@
-/****************************************************************************
-**
-** Copyright (C) 2018-2019 N7 Space sp. z o. o.
-** Contact: http://n7space.com
-**
-** This file is part of ASN.1/ACN Fuzzer - Tool for generating test cases
-** based on ASN.1/ACN models and simulating malformed or malicious data.
-**
-** Tool was developed under a programme and funded by
-** European Space Agency.
-**
-** This Tool is free software: you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation, either version 3 of the License, or
-** (at your option) any later version.
-**
-** This Tool is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program.  If not, see <http://www.gnu.org/licenses/>.
-**
-****************************************************************************/
+/** @file
+ * This file is part of the SpaceCreator.
+ *
+ * @copyright (C) 2021 N7 Space Sp. z o.o.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
 #pragma once
 
 #include <QString>
