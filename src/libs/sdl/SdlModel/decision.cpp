730330e85d17c96de409c4fa7f0edf719a3042b4
Mikolaj Owczarzak <mowczarzak@n7space.com>
2021-10-21 12:10:56 +0200

    SdlModel: added Decision stub

---
+++ src/libs/sdl/SdlModel/decision.cpp
@@ -0,0 +1,38 @@
+/** @file
+ * This file is part of the SpaceCreator.
+ *
+ * @copyright (C) 2021 N7 Space Sp. z o.o.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+#include "decision.h"
+
+#include <memory>
+
+namespace sdl {
+
+Decision::Decision() {}
+
+Expression *Decision::expression() const
+{
+    return m_expression.get();
+}
+
+void Decision::setExpression(std::unique_ptr<Expression> expression)
+{
+    m_expression = std::move(expression);
+}
+
+} // namespace sdl
