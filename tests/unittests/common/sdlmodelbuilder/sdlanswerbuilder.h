7d52a4e0b1b4bb68b21b20fdf7e9c7d51d446f8f
Mikolaj Owczarzak <mowczarzak@n7space.com>
2021-10-22 13:23:25 +0200

    SdlModelBuilder: added SdlAnswerBuilder

---
+++ tests/unittests/common/sdlmodelbuilder/sdlanswerbuilder.h
@@ -0,0 +1,51 @@
+/** @file
+ * This file is part of the SpaceCreator.
+ *
+ * @copyright (C) 2021 N7 Space Sp. z o.o.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+#pragma once
+
+#include <memory>
+#include <sdl/SdlModel/action.h>
+#include <sdl/SdlModel/answer.h>
+#include <sdl/SdlModel/variableliteral.h>
+
+using sdl::Action;
+using sdl::Answer;
+using sdl::Transition;
+using sdl::VariableLiteral;
+
+namespace tests::common {
+
+class SdlAnswerBuilder final
+{
+public:
+    SdlAnswerBuilder();
+
+    auto build() -> std::unique_ptr<Answer>;
+
+    auto withName(QString name) -> SdlAnswerBuilder &;
+
+    auto withAction(Action *action) -> SdlAnswerBuilder &;
+
+    auto withLiteral(VariableLiteral *literal) -> SdlAnswerBuilder &;
+
+private:
+    std::unique_ptr<Answer> m_answer;
+};
+
+} // namespace tests::common
