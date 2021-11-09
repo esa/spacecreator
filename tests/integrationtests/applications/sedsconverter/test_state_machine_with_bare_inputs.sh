ac4be48e050dd0a9261a857f85f374af186ed715
Michal Kurowski <mkurowski@n7space.com>
2021-11-09 23:36:38 +0100

    Added integration test script

--- tests/integrationtests/applications/sedsconverter/test_state_machine_with_bare_inputs.sh
+++ tests/integrationtests/applications/sedsconverter/test_state_machine_with_bare_inputs.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# TODO Set proper path
 SEDS_CONVERTER=../../../../../build/src/applications/sedsconverter/SedsConverter
+TEST_OUTPUT_DIR=output
 
-$SEDS_CONVERTER 
+# Setup output dir
+rm -r -f $TEST_OUTPUT_DIR
+mkdir -p $TEST_OUTPUT_DIR
+# Translate
+$SEDS_CONVERTER --from SEDS --to SDL --skip-validation -i resources/state_machine_with_bare_inputs.xml \
+    --sdl-filepath-prefix $TEST_OUTPUT_DIR/
+# Compare and clean-up on success
+diff $TEST_OUTPUT_DIR/Component.pr resources/state_machine_with_bare_inputs.output && rm -r -f $TEST_OUTPUT_DIR
