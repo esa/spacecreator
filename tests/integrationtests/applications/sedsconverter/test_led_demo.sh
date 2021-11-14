e8e1c02777b35239d0b697f1d26f53689c2edec7
Michal Kurowski <mkurowski@n7space.com>
2021-11-14 18:26:17 +0100

    Adding Led Demo integration test stub

---
+++ tests/integrationtests/applications/sedsconverter/test_led_demo.sh
@@ -0,0 +1,9 @@
+ sedsconverter --from SEDS --to ASN.1 --skip-validation -i resources/test_led_demo.xml \
+  --asn1-filepath-prefix output/ --acn-filepath-prefix output/
+sedsconverter --from SEDS --to InterfaceView --skip-validation -i resources/test_led_demo.xml \
+  --out output/led_demo.xml --iv-config config.xml
+sedsconverter --from SEDS --to SDL --skip-validation -i resources/test_led_demo.xml \
+  --sdl-filepath-prefix output/ --iv-config config.xml
+cp resources/test_led_demo.system_structure output/system_structure.pr
+cp resources/test_led_demo.dataviewuniq output/dataview-uniq.asn
+cd output && opengeode --toC system_structure.pr LedDemo.pr
