d6293970a10a349607217527467200b2ec0a587b
Michal Kurowski <mkurowski@n7space.com>
2021-11-26 02:34:40 +0100

    Added Asn1ToSeds test stub

---
+++ tests/integrationtests/applications/sedsconverter/asn1toseds/test_simple_data_types.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+SEDS_CONVERTER=$SPACECREATOR_BUILD_DIR/bin/sedsconverter
+# diff ignoring white space and blank lines
+DIFF="diff -w -B"
+TEST_OUTPUT_DIR=output
+
+# Setup output dir
+rm -r -f $TEST_OUTPUT_DIR
+mkdir -p $TEST_OUTPUT_DIR
+# Translate
+$SEDS_CONVERTER --from ASN.1 --to SEDS -i resources/test_simple_data_types.asn --out $TEST_OUTPUT_DIR/package.xml
---
+++ tests/integrationtests/applications/sedsconverter/asn1toseds/tst_sedsconverter_asn1toseds.cpp
@@ -0,0 +1,44 @@
+/** @file
+ * This file is part of the SpaceCreator.
+ *
+ * @copyright (C) 2021 N7 Space Sp. z o.o.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+#include <QObject>
+#include <QtTest>
+#include <stdlib.h>
+
+namespace sedsconverter {
+
+class tst_SedsConverter_Asn1ToSeds : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void testSimpleDataTypes();
+};
+
+void tst_SedsConverter_Asn1ToSeds::testSimpleDataTypes()
+{
+    const int result = system("./test_simple_data_types.sh");
+    QCOMPARE(result, 0);
+}
+
+} // namespace sedsconverter
+
+QTEST_MAIN(sedsconverter::tst_SedsConverter_Asn1ToSeds)
+
+#include "tst_sedsconverter_asn1toseds.moc"
