71f88333420b222cffa335a1f0480ede13569016
Filip Demski <fdemski@n7space.com>
2021-11-26 11:38:23 +0100

    Tests: Added missing IV config to the test_state_machine_with_bare_inputs

--- tests/integrationtests/applications/sedsconverter/sedstosdl/test_state_machine_with_bare_inputs.sh
+++ tests/integrationtests/applications/sedsconverter/sedstosdl/test_state_machine_with_bare_inputs.sh
@@ -12,6 +12,6 @@ rm -r -f $TEST_OUTPUT_DIR
 mkdir -p $TEST_OUTPUT_DIR
 # Translate
 $SEDS_CONVERTER --from SEDS --to SDL --skip-validation -i resources/state_machine_with_bare_inputs.xml \
-    --sdl-filepath-prefix $TEST_OUTPUT_DIR/
+    --iv-config resources/config.xml --sdl-filepath-prefix $TEST_OUTPUT_DIR/
 # Compare and clean-up on success
 diff $TEST_OUTPUT_DIR/Component.pr resources/state_machine_with_bare_inputs.output && rm -r -f $TEST_OUTPUT_DIR
