934045d42384959daa8ed0dc8ce048f185cc6e29
Filip Demski <fdemski@n7space.com>
2021-11-29 13:27:38 +0100

    IvToSedsTranslator: Integration tests for single interfaces

---
+++ tests/integrationtests/applications/sedsconverter/ivtoseds/test_cyclic_interface.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SEDS_CONVERTER=$SPACECREATOR_BUILD_DIR/bin/sedsconverter
+XML_VALIDATOR=$SPACECREATOR_BUILD_DIR/bin/xmlvalidator
+
+# diff ignoring white space and blank lines
+DIFF="diff -w -B"
+TEST_OUTPUT_DIR=output
+
+echo "Running SedsConverter test: ${0##*/}'"
+
+# Setup output dir and project
+rm -r -f $TEST_OUTPUT_DIR
+mkdir $TEST_OUTPUT_DIR
+
+# Translate
+$SEDS_CONVERTER --from InterfaceView --to SEDS -i resources/test_cyclic_interface.xml \
+    --out $TEST_OUTPUT_DIR/test_cyclic_interface.seds --iv-config resources/config.xml
+
+cd $TEST_OUTPUT_DIR
+
+cp ../resources/*.xsd . \
+  && $XML_VALIDATOR test_cyclic_interface.seds \
+  && $DIFF test_cyclic_interface.seds ../resources/test_cyclic_interface.output \
+  && cd .. \
+  && rm -r -f $TEST_OUTPUT_DIR
