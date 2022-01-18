0506304252b4037ca4963939ace1bfcb274c6d66
Mikolaj Owczarzak <mowczarzak@n7space.com>
2022-01-18 14:34:24 +0100

    TestGenerator: stub with failing test added

---
+++ tests/unittests/testgenerator/tst_testgenerator.cpp
@@ -0,0 +1,47 @@
+/** @file
+ * This file is part of the SpaceCreator.
+ *
+ * @copyright (C) 2022 N7 Space Sp. z o.o.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+#include <QObject>
+#include <QTest>
+#include <QtTest/qtestcase.h>
+#include <conversion/common/options.h>
+#include <csv/CsvImporter/csvimporter.h>
+#include <csv/CsvOptions/options.h>
+#include <qtestcase.h>
+
+namespace tests::testgenerator {
+
+class tst_testgenerator : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void testEmpty();
+};
+
+void tst_testgenerator::testEmpty()
+{
+    QFAIL("this shall happen");
+}
+
+} // namespace tests::testgenerator
+
+QTEST_MAIN(tests::testgenerator::tst_testgenerator)
+
+#include "tst_testgenerator.moc"
