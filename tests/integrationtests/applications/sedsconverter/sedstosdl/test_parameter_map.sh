566aafaf70bd913d4badd48aed3871d194306415
Michal Kurowski <mkurowski@n7space.com>
2022-02-01 17:23:38 +0100

    Added separate Requirer and Provider components

--- tests/integrationtests/applications/sedsconverter/sedstosdl/test_parameter_map.sh
+++ tests/integrationtests/applications/sedsconverter/sedstosdl/test_parameter_map.sh
@@ -19,16 +19,21 @@ $SEDS_CONVERTER --from SEDS --to SDL --aux-models ASN.1,InterfaceView --skip-val
   --iv-config resources/config.xml --asn1-filepath-prefix $TEST_OUTPUT_DIR/ --acn-filepath-prefix $TEST_OUTPUT_DIR/ \
   --sdl-filepath-prefix $TEST_OUTPUT_DIR/ --out $TEST_OUTPUT_DIR/interfaceview.xml
 # Setup additional data
-cp resources/test_parametermaphost.system_structure output/system_structure.pr
+cp resources/test_parametermaphostprovider.system_structure output/provider_system_structure.pr
+cp resources/test_parametermaphostrequirer.system_structure output/requirer_system_structure.pr
 cp $TEST_OUTPUT_DIR/PARAMETERS.asn $TEST_OUTPUT_DIR/dataview-uniq.asn
 # Rename the module to avoid naming conflicts
 sed -i 's/PARAMETERS/SYSTEM-DATAVIEW/g' $TEST_OUTPUT_DIR/dataview-uniq.asn
 cd $TEST_OUTPUT_DIR
 # Compare output against reference, and compile to make sure the reference is valid
 # Clean (rm) only if all steps pass
-$DIFF parametermaphost.pr ../resources/test_parametermaphost.output \
-  && $OPENGEODE --toAda system_structure.pr parametermaphost.pr \
-  && asn1scc -Ada --type-prefix asn1Scc dataview-uniq.asn parametermaphost_datamodel.asn \
-  && gcc -c parametermaphost.adb \
-  && cd .. \
-  && rm -r -f $TEST_OUTPUT_DIR
+$DIFF parametermaphostprovider.pr ../resources/test_parametermaphostprovider.output \
+  && $DIFF parametermaphostrequirer.pr ../resources/test_parametermaphostrequirer.output \
+  && $OPENGEODE --toAda provider_system_structure.pr parametermaphostprovider.pr \
+  && $OPENGEODE --toAda requirer_system_structure.pr parametermaphostrequirer.pr \
+  && asn1scc -Ada --type-prefix asn1Scc dataview-uniq.asn parametermaphostprovider_datamodel.asn \
+  && asn1scc -Ada --type-prefix asn1Scc dataview-uniq.asn parametermaphostrequirer_datamodel.asn \
+  && gcc -c parametermaphostprovider.adb \
+  && gcc -c parametermaphostrequirer.adb \
+  && cd .. 
+  #&& rm -r -f $TEST_OUTPUT_DIR
