c7102dcf1500f02c513f3f0e68025bc6d4cab920
Filip Demski <fdemski@n7space.com>
2022-03-11 11:53:38 +0100

    SedsToAsn1Translator: Creates skeleton for length entry test

---
+++ tests/integrationtests/applications/sedsconverter/sedstoasn1/test_length_entry.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SEDS_CONVERTER=$SPACECREATOR_BUILD_DIR/bin/sedsconverter
+# diff ignoring white space and blank lines
+DIFF="diff -w -B"
+TEST_OUTPUT_DIR=output
+
+echo "Running SedsConverter test: ${0##*/}'"
+
+# Setup output dir
+rm -r -f $TEST_OUTPUT_DIR
+mkdir -p $TEST_OUTPUT_DIR
+# Translate
+# gdb --args \
+$SEDS_CONVERTER --from SEDS --to ASN.1 --skip-validation -i resources/test_length_entry.xml \
+    --asn1-filepath-prefix $TEST_OUTPUT_DIR/ --acn-filepath-prefix $TEST_OUTPUT_DIR/
+# Compare and clean-up on success
+$DIFF $TEST_OUTPUT_DIR/COM-N7SPACE-LENGTHENTRY.asn resources/test_length_entry_asn.output && \
+    $DIFF $TEST_OUTPUT_DIR/COM-N7SPACE-LENGTHENTRY.acn resources/test_length_entry_acn.output && \
+    asn1scc -c -o $TEST_OUTPUT_DIR $TEST_OUTPUT_DIR/COM-N7SPACE-LENGTHENTRY.asn $TEST_OUTPUT_DIR/COM-N7SPACE-LENGTHENTRY.acn && \
+    rm -r -f $TEST_OUTPUT_DIR
