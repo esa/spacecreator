b85cbbef457e1155a565e56862e8927d50fddcab
Filip Demski <fdemski@n7space.com>
2022-03-14 14:03:31 +0100

    SedsToAsn1Translator: Removed array members from length entry test because of the asn1scc blocker

--- tests/integrationtests/applications/sedsconverter/sedstoasn1/test_length_entry.sh
+++ tests/integrationtests/applications/sedsconverter/sedstoasn1/test_length_entry.sh
@@ -15,13 +15,14 @@ mkdir -p $TEST_OUTPUT_DIR
 # Translate
 # gdb --args \
 $SEDS_CONVERTER --from SEDS --to ASN.1 --skip-validation -i resources/test_length_entry.xml \
-    --asn1-filepath-prefix $TEST_OUTPUT_DIR/ --acn-filepath-prefix $TEST_OUTPUT_DIR/
+    --asn1-filepath-prefix $TEST_OUTPUT_DIR/ --acn-filepath-prefix $TEST_OUTPUT_DIR/ \
+    --patcher-functions-filepath-prefix $TEST_OUTPUT_DIR/ --patcher-functions-header-filename lengthEntryPostEncoding
 
 cd $TEST_OUTPUT_DIR
 
 # Compare and clean-up on success
 $DIFF COM-N7SPACE-LENGTHENTRY.asn ../resources/test_length_entry_asn.output \
     && $DIFF COM-N7SPACE-LENGTHENTRY.acn ../resources/test_length_entry_acn.output \
-    && asn1scc -c -mfm com-n7space-lengthentry-postencoding COM-N7SPACE-LENGTHENTRY.asn COM-N7SPACE-LENGTHENTRY.acn \
+    && asn1scc -c -mfm lengthEntryPostEncoding COM-N7SPACE-LENGTHENTRY.asn COM-N7SPACE-LENGTHENTRY.acn \
     && gcc -o lengthentry ../resources/test_length_entry_main.c COM-N7SPACE-LENGTHENTRY.c asn1crt.c com-n7space-lengthentry-postencoding.c \
     && rm -r -f $TEST_OUTPUT_DIR
