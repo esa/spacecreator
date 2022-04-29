a7fa6702717e30af50a89086d656c3a538f8b62b
Michal Kurowski <mkurowski@n7space.com>
2022-04-29 15:39:36 +0200

    Observer tests are now outomated

---
+++ tests/integrationtests/applications/tmc/observers/tst_tmc_observers.cpp
@@ -0,0 +1,51 @@
+/** @file
+ * This file is part of the SpaceCreator.
+ *
+ * @copyright (C) 2022 N7 Space Sp. z o.o.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+#include <QObject>
+#include <QtTest>
+#include <stdlib.h>
+
+namespace tmc {
+
+class tst_Tmc_Observers : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void test_inputMonitoring();
+    void test_inputMonitoringFailure();
+};
+
+void tst_Tmc_Observers::test_inputMonitoring()
+{
+    const int result = system("./test_input_monitoring.sh");
+    QCOMPARE(result, 0);
+}
+
+void tst_Tmc_Observers::test_inputMonitoringFailure()
+{
+    const int result = system("./test_input_monitoring_failure.sh");
+    QCOMPARE(result, 0);
+}
+
+} // namespace sedsconverter
+
+QTEST_MAIN(tmc::tst_Tmc_Observers)
+
+#include "tst_tmc_observers.moc"
