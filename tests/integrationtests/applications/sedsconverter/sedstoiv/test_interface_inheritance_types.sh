eeb033dc69fc35e65de4b3067662412641de5348
Filip Demski <fdemski@n7space.com>
2022-05-12 12:09:44 +0200

    SedsToIvTranslator: Tests fixes

--- tests/integrationtests/applications/sedsconverter/sedstoiv/test_interface_inheritance_types.sh
+++ tests/integrationtests/applications/sedsconverter/sedstoiv/test_interface_inheritance_types.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 
 SEDS_CONVERTER=$SPACECREATOR_BUILD_DIR/bin/sedsconverter
+AADL_CONVERTER=$SPACECREATOR_BUILD_DIR/bin/aadlconverter
+UPDATE_DATAVIEW="asn2aadlPlus -f dataview-uniq.asn DataView.aadl -aadlv2"
 # diff ignoring white space and blank lines
 DIFF="diff -w -B"
 TEST_OUTPUT_DIR=output
@@ -22,6 +24,19 @@ $SEDS_CONVERTER --from SEDS --to InterfaceView --aux-models ASN.1 --skip-validat
 # Compare and clean-up on success
 cd $TEST_OUTPUT_DIR
 
+# Merge ASN files
+sed -e '$s/$/\n/' -s COM-N7SPACE-INTERFACEINHERITANCE1.asn \
+                     COM-N7SPACE-INTERFACEINHERITANCE2.asn COM-N7SPACE-INTERFACEINHERITANCE2-COMPONENT.asn> dataview-uniq.asn
+sed -e '$s/$/\n/' -s COM-N7SPACE-INTERFACEINHERITANCE1.acn \
+                     COM-N7SPACE-INTERFACEINHERITANCE2.acn COM-N7SPACE-INTERFACEINHERITANCE2-COMPONENT.acn> dataview-uniq.acn
+
 $DIFF interfaceview.xml ../resources/test_interface_inheritance_types.output \
+    && $UPDATE_DATAVIEW \
+    && $AADL_CONVERTER -o interfaceview.xml \
+    -t ../resources/xml2dv/interfaceview.tmplt \
+    -x DeploymentView.aadl \
+    && $AADL_CONVERTER -o interfaceview.xml \
+    -t ../resources/xml2iv/interfaceview.tmplt \
+    -x InterfaceView.aadl \
     && cd .. \
     && rm -r -f $TEST_OUTPUT_DIR
