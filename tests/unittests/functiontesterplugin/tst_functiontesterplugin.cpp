03b90d1153839bc2d516f3a59c92b1f17c83811d
Arkadiusz Koszewski <akoszewski@n7space.com>
2022-06-21 12:18:18 +0200

    FunctionTesterPlugin: Basic setup for plugin tests

---
+++ tests/unittests/functiontesterplugin/tst_functiontesterplugin.cpp
@@ -0,0 +1,45 @@
+/** @file
+ * This file is part of the SpaceCreator.
+ *
+ * @copyright (C) 2021-2022 N7 Space Sp. z o.o.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+#include <QObject>
+#include <QTest>
+#include <qtestcase.h>
+
+namespace tests::functiontesterplugin {
+
+class tst_functiontesterplugin : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void testCompilation();
+};
+
+void tst_functiontesterplugin::testCompilation()
+{
+    QString projectDirectory = QDir::currentPath();
+    QString generatedPath = projectDirectory + QDir::separator() + "generated";
+    // compileTests();
+}
+
+}
+
+QTEST_MAIN(tests::functiontesterplugin::tst_functiontesterplugin)
+
+#include "tst_functiontesterplugin.moc"
