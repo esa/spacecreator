17f8b1ef21d675b0d878976d49345421e263b4b8
Filip Demski <fdemski@n7space.com>
2022-06-21 09:31:01 +0200

    IvToPromelaTranslator: Input vector length limit test

---
+++ tests/integrationtests/applications/tmc/inputvectorlimit/test_input_vector_limit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TMC=$SPACECREATOR_BUILD_DIR/bin/tmc
+SPIN=spin
+CC=gcc
+
+TEST_OUTPUT_DIR=output
+RESOURCE_DIR=resources
+
+echo "Running TMC test: ${0##*/}'"
+
+# Setup output dir and project
+rm -r -f $TEST_OUTPUT_DIR
+mkdir $TEST_OUTPUT_DIR
+
+# Translate
+$TMC -iv $RESOURCE_DIR/interfaceview.xml \
+    -o $TEST_OUTPUT_DIR \
+    -ivl 10 \
+    -ivl rightinterface:4
+
+cd $TEST_OUTPUT_DIR \
+    && grep -q "for(inputVectorCounter : 0 \.\. 10)" system.pml \
+    && grep -q "for(inputVectorCounter : 0 \.\. 4)" system.pml \
+    && $SPIN -a system.pml \
+    && $CC -o system.out pan.c \
+    && cd .. \
+    && rm -r $TEST_OUTPUT_DIR
