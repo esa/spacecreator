e2edd481fbad7515e111a1c458ab58aeed71ccd9
Arkadiusz Koszewski <akoszewski@n7space.com>
2022-06-24 19:08:19 +0200

    FunctionTesterPlugin: Move backend functionality to a new TestGenerator class

--- src/qtcreator/functiontesterplugin/functiontesterplugin.cpp
+++ src/qtcreator/functiontesterplugin/functiontesterplugin.cpp
@@ -94,198 +94,9 @@ auto FunctionTesterPlugin::testUsingDataFromCsvGui() -> void
         return;
     }
     float delta = setDeltaDialog();
-    testUsingDataFromCsv(*interface, *csvModel, *asn1Model, delta);
-}
 
-auto FunctionTesterPlugin::initializePaths() -> void
-{
-    projectDirectory = getBaseDirectory();
-    generatedPath = projectDirectory + QDir::separator() + "generated";
-    generatedCodePath = generatedPath + QDir::separator() + "testdriver.c";
-    generatedIvPath = generatedPath + QDir::separator() + "interfaceview.xml";
-    generatedDvPath = generatedPath + QDir::separator() + "deploymentview.dv.xml";
-}
-
-auto FunctionTesterPlugin::getAllFunctionsFromModel(const ivm::IVModel &ivModel) -> std::vector<ivm::IVFunction *>
-{
-    QList<ivm::IVObject *> ivObjects = ivModel.visibleObjects();
-    std::vector<ivm::IVFunction *> ivFunctions = {};
-    for (const auto &ivObject : ivObjects) {
-        if (ivObject->isFunction()) {
-            ivFunctions.push_back(dynamic_cast<ivm::IVFunction *>(ivObject));
-        }
-    }
-    return ivFunctions;
-}
-
-auto FunctionTesterPlugin::prepareTestHarnessFiles(ivm::IVInterface &interface,
-        const csv::CsvModel &csvModel, Asn1Acn::Asn1Model &asn1Model) -> QString
-{
-    QDir dir(generatedPath);
-    dir.removeRecursively();
-    dir.mkpath(".");
-
-    std::stringstream outStream;
-    try {
-        outStream = TestDriverGenerator::generateTestDriver(csvModel, interface, asn1Model);
-        std::ofstream outFile(generatedCodePath.toStdString(), std::ofstream::out);
-        outFile << outStream.str();
-        outFile.close();
-    } catch (TestDriverGeneratorException &e) {
-        MessageManager::write(GenMsg::msgInfo.arg("TestDriverGeneratorException: " + QString(e.what())));
-        return {};
-    }
-
-    const auto ivModelGenerated = IvGenerator::generate(&interface);
-    if (ivModelGenerated == nullptr) {
-        MessageManager::write(GenMsg::msgInfo.arg("IV model was not generated"));
-        return {};
-    }
-
-    std::vector<ivm::IVFunction *> ivFunctions = getAllFunctionsFromModel(*ivModelGenerated);
-    exportIvModel(ivModelGenerated.get(), generatedIvPath);
-
-    const std::unique_ptr<dvm::DVModel> dvModelGenerated =
-            DvGenerator::generate(ivFunctions, "x86 Linux CPP", "x86_Linux_TestRunner", "Node_1", "hostPartition");
-    if (dvModelGenerated == nullptr) {
-        MessageManager::write(GenMsg::msgInfo.arg("DV model was not generated"));
-        return {};
-    }
-    exportDvModel(dvModelGenerated.get(), generatedDvPath);
-    constexpr int TESTED_FUNCTION_INDEX = 1;
-    return ivFunctions[TESTED_FUNCTION_INDEX]->title();
-}
-
-auto FunctionTesterPlugin::testUsingDataFromCsv(ivm::IVInterface &interface, const csv::CsvModel &csvModel,
-        Asn1Acn::Asn1Model &asn1Model, float delta) -> void
-{
-    initializePaths();
-    QString testedFunctionName = prepareTestHarnessFiles(interface, csvModel, asn1Model);
-    if (testedFunctionName.isEmpty()) {
-        return;
-    }
-    prepareTasteProjectSkeleton();
-    copyFunctionImplementations(testedFunctionName);
-    compileSystemUnderTest();
-}
-
-auto FunctionTesterPlugin::copyRecursively(const QString &srcPath, const QString &dstPath) -> bool
-{
-    QDir parentDstDir(QFileInfo(dstPath).path());
-    if (!parentDstDir.mkdir(QFileInfo(dstPath).fileName()))
-        return false;
-
-    QDir srcDir(srcPath);
-    foreach (const QFileInfo &info, srcDir.entryInfoList(QDir::Dirs | QDir::Files | QDir::NoDotAndDotDot)) {
-        QString srcItemPath = srcPath + "/" + info.fileName();
-        QString dstItemPath = dstPath + "/" + info.fileName();
-        if (info.isDir()) {
-            if (!copyRecursively(srcItemPath, dstItemPath)) {
-                return false;
-            }
-        } else if (info.isFile()) {
-            if (!QFile::copy(srcItemPath, dstItemPath)) {
-                return false;
-            }
-        } else {
-            qDebug() << "Unhandled item" << info.filePath() << "in copyRecursively";
-        }
-    }
-    return true;
-}
-
-auto FunctionTesterPlugin::runProcess(QString cmd, QStringList args, QString workingPath) -> void
-{
-    Process process(cmd, args, workingPath);
-    auto p = process.get();
-    p->waitForFinished(-1);
-    if (p->exitCode() != 0) {
-        qDebug() << "Error: " << p->exitCode() << p->readAllStandardError();
-        MessageManager::write(GenMsg::msgInfo.arg("Error: " + p->exitCode()));
-    } else {
-        MessageManager::write(GenMsg::msgInfo.arg("Command: " + cmd + " finished"));
-    }
-}
-
-auto FunctionTesterPlugin::prepareTasteProjectSkeleton() -> void
-{
-    MessageManager::write(GenMsg::msgInfo.arg("Preparing project skeleton..."));
-
-    auto copy = [this](const QFileInfo &file) {
-        if (file.suffix() == "asn" || file.suffix() == "acn" || file.fileName() == "Makefile") {
-            QFile::copy(file.absoluteFilePath(), generatedPath + QDir::separator() + file.fileName());
-        }
-    };
-
-    QFileInfoList qFileInfoList = QDir(projectDirectory).entryInfoList(QDir::Files);
-    std::for_each(qFileInfoList.cbegin(), qFileInfoList.cend(), copy);
-
-    runProcess("taste-update-data-view", QStringList() << "*.asn", generatedPath);
-    runProcess("make", QStringList() << "skeletons", generatedPath);
-}
-
-auto FunctionTesterPlugin::copyFunctionImplementations(const QString &functionName) -> void
-{
-    QString sourceFilePath = projectDirectory + QDir::separator() + "work" + QDir::separator() + functionName;
-    QString destFilePath = generatedPath + QDir::separator() + "work" + QDir::separator() + functionName;
-    copyRecursively(sourceFilePath, destFilePath);
-
-    sourceFilePath = generatedPath + QDir::separator() + "testdriver.c";
-    destFilePath = generatedPath + QDir::separator() + "work/testdriver/C/src/testdriver.c";
-    QFile::copy(sourceFilePath, destFilePath);
-}
-
-auto FunctionTesterPlugin::compileSystemUnderTest() -> void
-{
-    runProcess("make", QStringList() << "deploymentview" << "debug", generatedPath);
-    MessageManager::write(GenMsg::msgInfo.arg("Tests compilation finished"));
-}
-
-auto FunctionTesterPlugin::exportIvModel(ivm::IVModel *ivModel, const QString &outputFilename) -> void
-{
-    QByteArray modelData;
-    QBuffer modelDataBuffer(&modelData);
-    modelDataBuffer.open(QIODevice::WriteOnly);
-
-    IVExporter exporter;
-    exporter.exportObjects(ivModel->objects().values(), &modelDataBuffer);
-
-    QSaveFile outputFile(outputFilename);
-    outputFile.open(QIODevice::WriteOnly);
-    outputFile.write(modelData);
-    outputFile.commit();
-}
-
-auto FunctionTesterPlugin::exportDvModel(dvm::DVModel *dvModel, const QString &outputFilename) -> void
-{
-    const auto dvObjects = getDvObjectsFromModel(dvModel);
-
-    dve::DVExporter exporter;
-    QList<shared::VEObject *> objects;
-    std::for_each(dvObjects->begin(), dvObjects->end(), //
-            [&objects](const auto &obj) { objects.push_back(obj); });
-
-    const int objectMaxSize = 1'000;
-    QByteArray qba(objects.size() * objectMaxSize, '\00');
-    QBuffer buf = QBuffer(&qba);
-
-    exporter.exportObjects(objects, &buf);
-
-    QFile file(outputFilename);
-    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-        return;
-    }
-    file.write(qba);
-}
-
-auto FunctionTesterPlugin::getDvObjectsFromModel(dvm::DVModel *const model) -> std::unique_ptr<QVector<dvm::DVObject *>>
-{
-    auto generatedDvObjects = std::make_unique<QVector<dvm::DVObject *>>();
-    for (const auto &obj : model->objects()) {
-        generatedDvObjects->append(static_cast<dvm::DVObject *>(obj));
-    }
-
-    return generatedDvObjects;
+    TestGenerator testGenerator(getBaseDirectory());
+    testGenerator.testUsingDataFromCsv(*interface, *csvModel, *asn1Model, delta);
 }
 
 auto FunctionTesterPlugin::addTestInterfaceOption() -> void
@@ -300,7 +111,7 @@ auto FunctionTesterPlugin::addTestInterfaceOption() -> void
     acToolsFunctionTester->addAction(csvImport);
 }
 
-auto FunctionTesterPlugin::createActionContainerInTools(const QString &title) -> ActionContainer *
+auto FunctionTesterPlugin::createActionContainerInTools(const QString &title) -> Core::ActionContainer *
 {
     ActionContainer *const container = ActionManager::createMenu(Constants::M_TOOLS_FUNCTION_TESTER);
     QMenu *const functionTesterMenu = container->menu();
@@ -346,17 +157,6 @@ auto FunctionTesterPlugin::loadCsv() -> std::unique_ptr<csv::CsvModel>
     return ModelLoader::loadCsvModel(inputFilePath);
 }
 
-auto FunctionTesterPlugin::getCurrentIvEditorCore() -> IVEditorCorePtr
-{
-    auto *const currentDocument = EditorManager::currentDocument();
-    auto *const currentIvDocument = static_cast<IVEditorDocument *>(currentDocument);
-    if (currentIvDocument == nullptr) {
-        MessageManager::write(GenMsg::msgError.arg(GenMsg::ivFileNotSelected));
-        return nullptr;
-    }
-    return currentIvDocument->ivEditorCore();
-}
-
 auto FunctionTesterPlugin::getSelectedInterface() -> ivm::IVInterface *
 {
     IVEditorCorePtr ivEditorCorePtr = getCurrentIvEditorCore();
@@ -381,12 +181,6 @@ auto FunctionTesterPlugin::getSelectedInterface() -> ivm::IVInterface *
     return nullptr;
 }
 
-auto FunctionTesterPlugin::getBaseDirectory() -> QString
-{
-    QString ivDocumentPath = getCurrentIvEditorCore()->document()->path();
-    return QFileInfo(ivDocumentPath).absoluteDir().absolutePath();
-}
-
 auto FunctionTesterPlugin::loadAsn1Model() -> std::unique_ptr<Asn1Acn::Asn1Model>
 {
     QString baseDirectory = getBaseDirectory();
@@ -400,6 +194,23 @@ auto FunctionTesterPlugin::loadAsn1Model() -> std::unique_ptr<Asn1Acn::Asn1Model
         MessageManager::write(GenMsg::msgInfo.arg(tr("No ASN1 file found. Try to build the project first.")));
     }
     return modelPtr;
+}
+
+auto FunctionTesterPlugin::getBaseDirectory() -> QString
+{
+    QString ivDocumentPath = getCurrentIvEditorCore()->document()->path();
+    return QFileInfo(ivDocumentPath).absoluteDir().absolutePath();
+}
+
+auto FunctionTesterPlugin::getCurrentIvEditorCore() -> IVEditorCorePtr
+{
+    auto *const currentDocument = EditorManager::currentDocument();
+    auto *const currentIvDocument = static_cast<IVEditorDocument *>(currentDocument);
+    if (currentIvDocument == nullptr) {
+        qDebug() << GenMsg::ivFileNotSelected;
+        return nullptr;
+    }
+    return currentIvDocument->ivEditorCore();
 }
 
 } // namespace spctr
