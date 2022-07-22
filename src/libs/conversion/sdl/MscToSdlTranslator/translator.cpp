2e06175a8c8994fb5cde7fc6295f0c733fb51996
Filip Demski <fdemski@n7space.com>
2022-07-22 13:22:51 +0200

    MscToSdlTranslator: Created skeleton for MSC to SDL translator

---
+++ src/libs/conversion/sdl/MscToSdlTranslator/translator.cpp
@@ -0,0 +1,68 @@
+/** @file
+ * This file is part of the SpaceCreator.
+ *
+ * @copyright (C) 2022 N7 Space Sp. z o.o.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+#include "translator.h"
+
+#include <conversion/common/translation/exceptions.h>
+
+using conversion::translator::TranslationException;
+using msc::MscModel;
+using sdl::SdlModel;
+
+namespace conversion::sdl::translator {
+
+std::vector<std::unique_ptr<Model>> MscToSdlTranslator::translateModels(
+        std::vector<Model *> sourceModels, const Options &options) const
+{
+    Q_UNUSED(options);
+
+    checkSourceModelCount(sourceModels);
+
+    const auto *mscModel = getModel<MscModel>(sourceModels);
+
+    return translateMscModel(mscModel);
+}
+
+ModelType MscToSdlTranslator::getSourceModelType() const
+{
+    return ModelType::Msc;
+}
+
+ModelType MscToSdlTranslator::getTargetModelType() const
+{
+    return ModelType::Sdl;
+}
+
+std::set<ModelType> MscToSdlTranslator::getDependencies() const
+{
+    static std::set<ModelType> dependencies { ModelType::Msc };
+    return dependencies;
+}
+
+std::vector<std::unique_ptr<Model>> MscToSdlTranslator::translateMscModel(const MscModel *mscModel) const
+{
+    auto sdlModel = std::make_unique<SdlModel>();
+
+    std::vector<std::unique_ptr<Model>> result;
+    result.push_back(std::move(sdlModel));
+
+    return result;
+}
+
+} // namespace conversion::sdl::translator
---
+++ src/libs/conversion/sdl/MscToSdlTranslator/translator.h
@@ -0,0 +1,75 @@
+/** @file
+ * This file is part of the SpaceCreator.
+ *
+ * @copyright (C) 2022 N7 Space Sp. z o.o.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+
+#pragma once
+
+#include <conversion/common/translation/translator.h>
+#include <msccore/mscmodel.h>
+#include <sdl/SdlModel/sdlmodel.h>
+
+namespace conversion::sdl::translator {
+
+/**
+ * @brief   Translator between MSC model and SDL model
+ */
+class MscToSdlTranslator final : public ::conversion::translator::Translator
+{
+public:
+    /**
+     * @brief   Translate given MSC models into an SDL model
+     *
+     * @param   sourceModels    List of models
+     * @param   options         List of options
+     *
+     * @return  SDL model translated from the MSC models
+     */
+    virtual auto translateModels(std::vector<Model *> sourceModels, const Options &options) const
+            -> std::vector<std::unique_ptr<Model>> override;
+
+    /**
+     * @brief   Return a model type that is a source of this translator
+     *
+     * @return  Source model type
+     */
+    virtual auto getSourceModelType() const -> ModelType override;
+    /**
+     * @brief   Returns a model type that is a target of this translator
+     *
+     * @return  Target model type
+     */
+    virtual auto getTargetModelType() const -> ModelType override;
+    /**
+     * @brief   Provides a set of all source model types that are required for the translation
+     *
+     * @return  Set of required models
+     */
+    virtual auto getDependencies() const -> std::set<ModelType> override;
+
+private:
+    /**
+     * @brief   Translate MSC model
+     *
+     * @param   mscModel    MSC model to translate
+     *
+     * @return  Result SDL model
+     */
+    auto translateMscModel(const ::msc::MscModel *mscModel) const -> std::vector<std::unique_ptr<Model>>;
+};
+
+} // namespace conversion::sdl::translator
