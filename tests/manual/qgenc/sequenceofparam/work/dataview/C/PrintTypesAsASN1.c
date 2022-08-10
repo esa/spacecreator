e248b9405cd5d663979a848a904fdc7db46f9045
Arkadiusz Koszewski <akoszewski@n7space.com>
2022-08-10 14:52:42 +0200

    Add skeletons of example projects for manual tests

---
+++ tests/manual/qgenc/sequenceofparam/work/dataview/C/PrintTypesAsASN1.c
@@ -0,0 +1,183 @@
+#ifdef __unix__
+#include <stdio.h>
+#endif
+
+#include "PrintTypesAsASN1.h"
+
+#ifdef __linux__
+#include <pthread.h>
+
+static pthread_mutex_t g_printing_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+#endif
+
+void PrintASN1MyInteger(const char *paramName, const asn1SccMyInteger *pData)
+{
+    (void)paramName;
+    (void)pData;
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s MyInteger ::= ", paramName);
+    printf("%s ", paramName);
+    #if WORD_SIZE==8
+    printf("%"PRId64, (*pData));
+    #else
+    printf("%d", (*pData));
+    #endif
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1MySeqOf(const char *paramName, const asn1SccMySeqOf *pData)
+{
+    (void)paramName;
+    (void)pData;
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s MySeqOf ::= ", paramName);
+    printf("%s ", paramName);
+    {
+        int i1;
+        printf("{");
+        for(i1=0; i1<3; i1++) {
+            if (i1) 
+                printf(",");
+            #if WORD_SIZE==8
+            printf("%"PRId64, (*pData).arr[i1]);
+            #else
+            printf("%d", (*pData).arr[i1]);
+            #endif
+        }
+        printf("}");
+    }
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1T_Int32(const char *paramName, const asn1SccT_Int32 *pData)
+{
+    (void)paramName;
+    (void)pData;
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s T-Int32 ::= ", paramName);
+    printf("%s ", paramName);
+    #if WORD_SIZE==8
+    printf("%"PRId64, (*pData));
+    #else
+    printf("%d", (*pData));
+    #endif
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1T_UInt32(const char *paramName, const asn1SccT_UInt32 *pData)
+{
+    (void)paramName;
+    (void)pData;
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s T-UInt32 ::= ", paramName);
+    printf("%s ", paramName);
+    #if WORD_SIZE==8
+    printf("%"PRId64, (*pData));
+    #else
+    printf("%d", (*pData));
+    #endif
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1T_Int8(const char *paramName, const asn1SccT_Int8 *pData)
+{
+    (void)paramName;
+    (void)pData;
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s T-Int8 ::= ", paramName);
+    printf("%s ", paramName);
+    #if WORD_SIZE==8
+    printf("%"PRId64, (*pData));
+    #else
+    printf("%d", (*pData));
+    #endif
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1T_UInt8(const char *paramName, const asn1SccT_UInt8 *pData)
+{
+    (void)paramName;
+    (void)pData;
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s T-UInt8 ::= ", paramName);
+    printf("%s ", paramName);
+    #if WORD_SIZE==8
+    printf("%"PRId64, (*pData));
+    #else
+    printf("%d", (*pData));
+    #endif
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1T_Boolean(const char *paramName, const asn1SccT_Boolean *pData)
+{
+    (void)paramName;
+    (void)pData;
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s T-Boolean ::= ", paramName);
+    printf("%s ", paramName);
+    printf("%s", (int)(*pData)?"TRUE":"FALSE");
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1T_Null_Record(const char *paramName, const asn1SccT_Null_Record *pData)
+{
+    (void)paramName;
+    (void)pData;
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s T-Null-Record ::= ", paramName);
+    printf("%s ", paramName);
+    printf("{");
+    printf("}");
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
