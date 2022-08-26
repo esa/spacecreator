d8e2012c4780ae34e9bac1f851c8fad0a5459f66
Igor Jarek <ijarek@n7space.com>
2022-08-26 18:12:22 +0200

    SimulinkToAsn1Translator: fixed format check

--- src/libs/conversion/asn1/SimulinkToAsn1Translator/specialized/portdatatypetranslator.h
+++ src/libs/conversion/asn1/SimulinkToAsn1Translator/specialized/portdatatypetranslator.h
@@ -27,8 +27,8 @@
 namespace conversion::asn1::translator {
 
 /**
- * @brief   When port (inport or outport) has "Port Dimensions" parameter different from -1 or greater than 1 or it is a vector
- * [n m], then new ASN1 data type has to be created.
+ * @brief   When port (inport or outport) has "Port Dimensions" parameter different from -1 or greater than 1 or it is a
+ * vector [n m], then new ASN1 data type has to be created.
  */
 class PortDataTypeTranslator final
 {
