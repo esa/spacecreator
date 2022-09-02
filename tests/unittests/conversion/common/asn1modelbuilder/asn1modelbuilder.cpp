64631e262dd568a83fb867225deee395efe35b5a
Igor Jarek <ijarek@n7space.com>
2022-09-02 15:27:15 +0200

    SimulinkToIVTranslator: extended asn1modelbuilder with new types

--- tests/unittests/conversion/common/asn1modelbuilder/asn1modelbuilder.cpp
+++ tests/unittests/conversion/common/asn1modelbuilder/asn1modelbuilder.cpp
@@ -19,11 +19,13 @@
 
 #include "asn1modelbuilder.h"
 
+#include <asn1library/asn1/constraints/rangeconstraint.h>
 #include <asn1library/asn1/definitions.h>
 #include <asn1library/asn1/typeassignment.h>
-#include <asn1library/asn1/types/integer.h>
+#include <asn1library/asn1/types/boolean.h>
 
 using namespace Asn1Acn;
+using namespace Asn1Acn::Constraints;
 using namespace Asn1Acn::Types;
 
 namespace tests::conversion::common {
@@ -47,12 +49,61 @@ std::unique_ptr<Asn1Model> Asn1ModelBuilder::build()
     return std::make_unique<Asn1Model>(std::move(files));
 }
 
-Asn1ModelBuilder &Asn1ModelBuilder::withIntegerDataType(QString name)
+Asn1ModelBuilder &Asn1ModelBuilder::withBooleanDataType(QString name, QString trueValue, QString falseValue)
 {
-    auto type = std::make_unique<Asn1Acn::Types::Integer>(name);
-    type->setEncoding(IntegerEncoding::pos_int);
-    type->setEndianness(Endianness::little);
-    type->setSize(64);
+    auto type = std::make_unique<Boolean>(name);
+
+    type->setTrueValue(trueValue);
+    type->setFalseValue(falseValue);
+
+    auto typeAssignment =
+            std::make_unique<Asn1Acn::TypeAssignment>(name, name, Asn1Acn::SourceLocation(), std::move(type));
+
+    m_definitions->addType(std::move(typeAssignment));
+
+    return *this;
+}
+
+Asn1ModelBuilder &Asn1ModelBuilder::withRealDataType(
+        QString name, Endianness endianness, RealEncoding realEncoding, double min, double max)
+{
+    auto type = std::make_unique<Real>(name);
+
+    type->setEndianness(endianness);
+    type->setEncoding(realEncoding);
+
+    auto rangeConstraint = RangeConstraint<RealValue>::create({ min, max });
+    type->constraints().append(std::move(rangeConstraint));
+
+    auto typeAssignment =
+            std::make_unique<Asn1Acn::TypeAssignment>(name, name, Asn1Acn::SourceLocation(), std::move(type));
+
+    m_definitions->addType(std::move(typeAssignment));
+
+    return *this;
+}
+
+Asn1ModelBuilder &Asn1ModelBuilder::withIntegerDataType(QString name, int size, int acnMinSize, int acnMaxSize,
+        Endianness endianness, IntegerEncoding integerEncoding, int64_t min, int64_t max)
+{
+    auto type = std::make_unique<Integer>(name);
+
+    type->setSize(size);
+    type->setEndianness(endianness);
+    type->setEncoding(integerEncoding);
+
+    if (acnMinSize != 0) {
+        type->setAcnMinSizeInBits(acnMinSize);
+    }
+
+    if (acnMaxSize != 0) {
+        type->setAcnMaxSizeInBits(acnMaxSize);
+    }
+
+    if (min != 0 || max != 0) {
+        auto rangeConstraint = RangeConstraint<IntegerValue>::create({ min, max });
+        type->constraints().append(std::move(rangeConstraint));
+    }
 
     auto typeAssignment =
             std::make_unique<Asn1Acn::TypeAssignment>(name, name, Asn1Acn::SourceLocation(), std::move(type));
