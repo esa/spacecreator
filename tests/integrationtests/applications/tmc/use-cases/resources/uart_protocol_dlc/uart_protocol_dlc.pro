be7f53cfd8935314d6d4e9b235d2dab915f618e6
Michal Kurowski <mkurowski@n7space.com>
2022-09-13 15:09:49 +0200

    Update to the current TASTE version

--- tests/integrationtests/applications/tmc/use-cases/resources/uart_protocol_dlc/uart_protocol_dlc.pro
+++ tests/integrationtests/applications/tmc/use-cases/resources/uart_protocol_dlc/uart_protocol_dlc.pro
@@ -2,18 +2,18 @@ TEMPLATE = lib
 CONFIG -= qt
 CONFIG += generateC
 
-DISTFILES +=  $(HOME)/tool-inst/share/taste-types/taste-types.asn \
-    data_model/basic_types.asn \
-    data_model/bilevel_types.asn \
-    data_model/uart_dlc.asn
-DISTFILES += uart_protocol_dlc.asn
-DISTFILES += uart_protocol_dlc.acn
+DISTFILES +=  $(HOME)/tool-inst/share/taste-types/taste-types.asn
 DISTFILES += uart_protocol_dlc.msc
 DISTFILES += interfaceview.xml
 DISTFILES += work/binaries/*.msc
 DISTFILES += work/binaries/coverage/index.html
 DISTFILES += work/binaries/filters
-#include(handleAsn1AcnBuild.pri)
+
+DISTFILES += data_model/basic_types.asn
+DISTFILES += data_model/bilevel_types.asn
+DISTFILES += data_model/uart_dlc.asn
+DISTFILES += uart_protocol_dlc.asn
+DISTFILES += uart_protocol_dlc.acn
 include(work/taste.pro)
 message($$DISTFILES)
 
---
+++ tests/integrationtests/applications/tmc/use-cases/test_uart.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TMC=$SPACECREATOR_BUILD_DIR/bin/tmc
+SPIN=spin
+CC=gcc
+
+# diff ignoring white space and blank lines
+DIFF="diff -w -B"
+TEST_OUTPUT_DIR=output_uart
+RESOURCE_DIR=resources/uart_protocol_dlc
+SUBTYPES_DIR=${RESOURCE_DIR}/work/modelchecking/subtypes
+OBSERVERS_DIR=${RESOURCE_DIR}/work/modelchecking/properties/observers
+PROPERTIES_DIR=${RESOURCE_DIR}/work/modelchecking/properties
+
+echo "Running TMC test: ${0##*/}'"
+
+# Setup output dir and project
+rm -r -f $TEST_OUTPUT_DIR
+mkdir $TEST_OUTPUT_DIR
+
+# Translate
+$TMC -iv $RESOURCE_DIR/interfaceview.xml \
+     -o $TEST_OUTPUT_DIR
+#    -ivl 1 
+#    -sub ${SUBTYPES_DIR}/subtypes.asn \
+#    -scl ${PROPERTIES_DIR}/sc.scl \
+#    -os ${PROPERTIES_DIR}/InputObserver/InputObserver.pr \
+#    -os ${PROPERTIES_DIR}/OutputObserver/OutputObserver.pr \
+
+# Compile the actual Spin model checker. This tests
+# whether all the features are supported, and that
+# one feature does not interfere with another.
+# The model is not checked, as it would take a huge amounth of time,
+# potentially infinite given no signal generation optimizations.
+# Checking could be added in the future, once the optimizations
+# are implemented.
+cd $TEST_OUTPUT_DIR \
+    && $SPIN -a system.pml \
+    && $CC -DVECTORSZ=65536 -o system.out pan.c \
+    && cd .. \
+    && rm -r $TEST_OUTPUT_DIR
