d5e0766f0d896867895d87355e6cb86c93427210
Filip Demski <fdemski@n7space.com>
2022-09-23 13:50:12 +0200

    Asn1ToPromelaTranslator: Renamed subtyping test for fixed sized strings

--- tests/integrationtests/applications/tmc/subtyping/test_ia5string_fixed_size.sh
+++ tests/integrationtests/applications/tmc/subtyping/test_ia5string_fixed_size.sh
@@ -9,7 +9,7 @@ CC=gcc
 # diff ignoring white space and blank lines
 DIFF="diff -w -B"
 TEST_OUTPUT_DIR=output
-RESOURCES_DIR=resources/ia5string
+RESOURCES_DIR=resources/ia5string_fixed_size
 PROPERTIES_DIR=$RESOURCES_DIR/work/modelchecking/properties
 
 echo "Running TMC test: ${0##*/}"
