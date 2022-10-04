8fb2431cfc8cd6cb38e11e27e923e7b58ff6cd12
Filip Demski <fdemski@n7space.com>
2022-10-04 16:11:34 +0200

    SedsConverter: Added parameters to transaction test

--- tests/integrationtests/applications/sedsconverter/sedstosdl/test_transaction.sh
+++ tests/integrationtests/applications/sedsconverter/sedstosdl/test_transaction.sh
@@ -30,6 +30,7 @@ cp $TEST_OUTPUT_DIR/TRANSACTION.asn $TEST_OUTPUT_DIR/dataview-uniq.asn
 sed -i 's/TRANSACTION/SYSTEM-DATAVIEW/g' $TEST_OUTPUT_DIR/dataview-uniq.asn
 
 cd $TEST_OUTPUT_DIR
+
 # Compare output against reference, and compile to make sure the reference is valid
 # Clean (rm) only if all steps pass
 # This test uses Ada, as C backend in OpenGEODE is too buggy to handle this example
@@ -37,9 +38,7 @@ $DIFF pinger.pr ../resources/test_transaction_pinger.output \
     && $DIFF ponger.pr ../resources/test_transaction_ponger.output \
     && $OPENGEODE --toAda test_transaction_pinger.system_structure pinger.pr \
     && asn1scc -Ada --type-prefix asn1Scc -equal dataview-uniq.asn pinger_datamodel.asn \
-    && gcc -c pinger.adb \
     && $OPENGEODE --toAda test_transaction_ponger.system_structure ponger.pr \
     && asn1scc -Ada --type-prefix asn1Scc -equal dataview-uniq.asn ponger_datamodel.asn \
-    && gcc -c ponger.adb \
     && cd .. \
     && rm -r -f $TEST_OUTPUT_DIR
