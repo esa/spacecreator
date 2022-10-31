acf1240179c2e9968cb9fd1fe110eb544a15d4e9
Arkadiusz Koszewski <akoszewski@n7space.com>
2022-10-31 14:49:58 +0100

    TMC: Add new test for constraints check for real type

--- tests/integrationtests/applications/tmc/types/test_real_constraints.sh
+++ tests/integrationtests/applications/tmc/types/test_real_constraints.sh
@@ -9,7 +9,7 @@ CC=gcc
 # diff ignoring white space and blank lines
 DIFF="diff -w -B"
 TEST_OUTPUT_DIR=output
-RESOURCE_DIR=resources/real
+RESOURCE_DIR=resources/real-constraints
 PROPERTIES_DIR=$RESOURCE_DIR/work/modelchecking/properties
 
 echo "Running TMC test: ${0##*/}"
@@ -19,7 +19,7 @@ mkdir $TEST_OUTPUT_DIR
 
 # Translate
 $TMC -iv $RESOURCE_DIR/interfaceview.xml \
-    -scl $PROPERTIES_DIR/StopConditions/Finish.scl \
+    -scl $PROPERTIES_DIR/StopConditions/Overflow.scl \
     -o $TEST_OUTPUT_DIR \
     -enable-reals
 
@@ -27,22 +27,8 @@ cd $TEST_OUTPUT_DIR \
     && $SPIN -a system.pml \
     && $CC -o system.out pan.c \
     && ./system.out -m1000000 -a -n > system.output \
-    && grep -q "errors: 0" system.output \
-    && cd ..
-
-rm -rf $TEST_OUTPUT_DIR
-mkdir $TEST_OUTPUT_DIR
-
-$TMC -iv $RESOURCE_DIR/interfaceview.xml \
-    -scl $PROPERTIES_DIR/StopConditions/Failure.scl \
-    -o $TEST_OUTPUT_DIR \
-    -enable-reals
-
-cd $TEST_OUTPUT_DIR \
-    && $SPIN -a system.pml \
-    && $CC -o system.out pan.c \
-    && ./system.out -m1000000 -a -n > system.output \
-    && grep -q "errors: 0" system.output \
+    && grep -q "errors: 1" system.output \
+    && grep -q "assertion violated  !(((global_state.controller.param>=" system.output \
     && cd ..
 
 rm -rf $TEST_OUTPUT_DIR
