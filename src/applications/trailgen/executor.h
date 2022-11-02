9dd3c22418e1223246a8a4dd295b5bdbd0a8b36f
Rafał Babski <rbabski@n7space.com>
2022-11-02 10:58:45 +0100

    Tmc: Refactor and docs in applications tmc and trailgen

--- src/applications/trailgen/executor.h
+++ src/applications/trailgen/executor.h
@@ -20,24 +20,40 @@
 #pragma once
 
 #include <QObject>
+#include <memory>
 #include <tmc/TmcConverter/converter.h>
 
 namespace tmc {
+/**
+ * @brief TrailgenExecutor creates TmcConverter and starts conversion from
+ * taste project and spin trail files to simulator files.
+ * After conversion finishes it quits QCoreApplication.
+ * During conversion the messages are reported on stdout.
+ */
 class TrailgenExecutor final : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Constructor.
+     *
+     * @param parent, parent of the object.
+     */
     TrailgenExecutor(QObject *parent = nullptr);
-    ~TrailgenExecutor();
 
 public Q_SLOTS:
+    /**
+     * @brief Start the execution of verification.
+     */
     void execute();
+
+public Q_SLOTS:
     void message(QString text);
     void finished(bool success);
     void start();
 
 private:
-    tmc::converter::TmcConverter *m_converter;
+    std::unique_ptr<tmc::converter::TmcConverter> m_converter;
     QString m_inputSpinTrailFilepath;
     QString m_outputSimulatorTrailFilepath;
 };
