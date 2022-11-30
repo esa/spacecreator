786df54e510dfd95a3eb7f3c8bf2997b773fc71a
Guenter Schwann <guenter@vikingsoftware.com>
2022-11-30 12:46:12 +0100

    Cmake sets test ouput to be in xml

--- cmake/TestUtils.cmake
+++ cmake/TestUtils.cmake
@@ -1,6 +1,14 @@
 # Functions supporting tests
 #
 
+# This is to be used instead of cmake built in "add_test()"
+# It adds command parameters for the test output
+#
+function(add_testXML TEST_NAME)
+    add_test(NAME ${TEST_NAME} COMMAND ${TEST_NAME} -o ${CMAKE_BINARY_DIR}/${TEST_NAME}.xml,xunitxml)
+endfunction()
+
+
 # Create one test that is called "TEST_NAME" and has a test source TEST_NAME.cpp
 # LIBRARY is the LIBRARY (a list is possible as well) the test depends on
 # Optionally you can add a list of aditional source files
@@ -21,7 +29,7 @@ function(addQtTest TEST_NAME LIBRARY)
 
     target_compile_definitions(${TEST_NAME} PUBLIC EXAMPLES_DIR=\"${CMAKE_SOURCE_DIR}/examples/\")
 
-    add_test(NAME ${TEST_NAME} COMMAND ${TEST_NAME} -xunitxml -o ${CMAKE_BINARY_DIR}/${TEST_NAME}.xml)
+    add_testXML(${TEST_NAME})
 
     if(OPTIONS_NO_DEPRECATED)
         target_compile_options(${TEST_NAME} PRIVATE -Wno-deprecated -Wno-deprecated-declarations)
