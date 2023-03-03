f873853d0b8ea6611bcc5dbed870609636294bbb
Arkadiusz Koszewski <akoszewski@n7space.com>
2023-03-03 11:33:26 +0100

    Review fixes

--- src/libs/common/exceptions/inconsistentmodelexception.h
+++ src/libs/common/exceptions/inconsistentmodelexception.h
@@ -2,14 +2,36 @@
 #define INCONSISTENT_MODEL_EXCEPTION
 
 #include <exception>
+#include <QString>
 
+/**
+ * @brief Inconsistent model exception class
+ */
 class InconsistentModelException : public std::exception
 {
 public:
-	const char * what() const throw()
+    /**
+     * @brief   Constructor
+     *
+     * @param   message     Error message
+     */
+    explicit InconsistentModelException(const QString& message = "Inconsistent Model Exception")
+        : m_message(message)
+    {}
+
+    /**
+     * @brief   Returns error message as const char*
+     *
+     * @returns Error message
+     */
+    const char * what() const noexcept
     {
-    	return "Inconsistent Model Exception";
+    	return m_message.toStdString().c_str();
     }
+
+private:
+    /** @brief  Error message */
+    QString m_message;
 };
 
 #endif
