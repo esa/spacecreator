879e4f6e0dfd8a3e656430403bef19c6e10b2103
Kasper <kasper@vikingsoftware.com>
2023-03-29 11:02:43 +0200

    removed comment

--- src/libs/libiveditor/itemeditor/ivitemmodel.h
+++ src/libs/libiveditor/itemeditor/ivitemmodel.h
@@ -48,7 +48,7 @@ public:
     enum class Roles
     {
         IdRole = Qt::UserRole,
-        ItemVisible, // Visible in this context means "Item is visible at root level or in a nested function"
+        ItemVisible,
     };
     Q_ENUM(Roles);
 
