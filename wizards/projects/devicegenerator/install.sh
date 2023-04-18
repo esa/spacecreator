0d6bff342080f039f9d1515cdde5a09fe22f3608
Filip Florczyk <filip.florczyk@n7space.com>
2023-04-18 09:33:35 +0200

    Update CD installer script

--- wizards/projects/devicegenerator/install.sh
+++ wizards/projects/devicegenerator/install.sh
@@ -3,8 +3,13 @@
 PROJECT_NAME=$(xmllint --xpath "/*/@projectName" interfaceview.xml | sed -e "s/projectName=//g" | sed -e "s/\\"//g")
 PARTITION_NAME=$(echo ${PROJECT_NAME,,})
 
+TOOL_INST=$(taste-config --prefix)
 CD_INSTALLATION_DIRECTORY=$COMMUNICATION_DEVICE_PATH/%{CDBoard}/port_%{ProjectNameLowerCase}
 FUNCTION_NAMES=$(xmllint --xpath "//*[local-name()='Function']/@name" interfaceview.xml)
+
+DISTFILES=$(qmake %{ProjectNameLowerCase}.pro -o /tmp/null 2>&1)
+ASN1_FILES_WITHOUT_CONFIG=$(find ${DISTFILES} 2>/dev/null | egrep '.asn$$|.asn1$$' | grep -v 'configuration.asn')
+ASN1_CONFIG_FILE=$(find ${DISTFILES} 2>/dev/null | egrep '.asn$$|.asn1$$' | grep 'configuration.asn')
 
 mkdir -p $CD_INSTALLATION_DIRECTORY
 cp -r snippets/. $CD_INSTALLATION_DIRECTORY
@@ -24,18 +29,33 @@ THREAD_FILES=$(find work/build/**/${PARTITION_NAME} -name "thread_*" -a -not -na
 INTERFACE_FILES=$(find work/build/**/${PARTITION_NAME} -name "${PARTITION_NAME}_interface.h" -o -name "${PARTITION_NAME}_shared_interface.cc")
 TRANSPORT_FILES=$(find work/build/**/${PARTITION_NAME} -name "driver_transport.*")
 ROUTING_FILES=$(find work/build/**/${PARTITION_NAME} -name "routing.*")
-DATA_VIEW_FILES=$(find work/dataview/C -name "dataview-uniq.*")
 REQUEST_SIZE_FILE=work/build/**/${PARTITION_NAME}/request_size.h
 SYSTEM_CONFIG_FILE=work/build/system_config.h
 
 ALL_SOURCE_FILES=( "${ASN1_TYPES_H_FILES} ${ASN1_TYPES_C_FILES} ${H_FILES} ${C_FILES} ${CC_FILES} ${ADS_FILES} \\
 ${ADB_FILES} ${DRIVER_INTERFACE_FILES} ${THREAD_FILES} ${INTERFACE_FILES} ${TRANSPORT_FILES} ${ROUTING_FILES} \\
-${DATA_VIEW_FILES} ${REQUEST_SIZE_FILE} ${SYSTEM_CONFIG_FILE}" )
+${REQUEST_SIZE_FILE} ${SYSTEM_CONFIG_FILE}" )
 
 for SOURCE_FILE_NAME in $ALL_SOURCE_FILES; do
     cp ${SOURCE_FILE_NAME} $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}/
 done
 
+CD_DATAVIEW_DIR=${CD_INSTALLATION_DIRECTORY}/%{ProjectNameLowerCase}/dataview
+ASN1_CONFIG_XML=${CD_DATAVIEW_DIR}/%{ProjectNameLowerCase}Configuration.xml
+
+mkdir ${CD_DATAVIEW_DIR}
+asn1scc --xml-ast ${ASN1_CONFIG_XML} %{ProjectName}configuration.asn
+ASN1_CONFIG_DATA_TYPES=$(xmllint --xpath "//ExportedType/@Name" ${ASN1_CONFIG_XML} | sed -e "s/Name=//g" | sed -e "s/\\"//g" | sed -e "s/-/_/g")
+cat ${ASN1_FILES_WITHOUT_CONFIG} ${TOOL_INST}/share/taste-types/taste-types.asn work/system.asn > ${CD_DATAVIEW_DIR}/dataview-uniq.asn
+sed -i "s/END/END\\n/g" ${CD_DATAVIEW_DIR}/dataview-uniq.asn
+asn1scc -o ${CD_DATAVIEW_DIR} -typePrefix asn1Scc -renamePolicy 3 -equal -fp AUTO -c ${CD_DATAVIEW_DIR}/dataview-uniq.asn
+cp ${CD_DATAVIEW_DIR}/dataview-uniq.h $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}
+cp ${CD_DATAVIEW_DIR}/dataview-uniq.c $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}
+rm -r ${CD_DATAVIEW_DIR}
+
+for ASN1_CONFIG_DATA_TYPE in $ASN1_CONFIG_DATA_TYPES; do
+    find $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase} -type f | xargs sed -i "s/asn1Scc${ASN1_CONFIG_DATA_TYPE}/${ASN1_CONFIG_DATA_TYPE}/g"
+done
 find $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase} -type f | xargs sed -i "s/asn1Scc/${PARTITION_NAME}_asn1Scc/g"
 find $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase} -type f | xargs sed -i "s/request_size.h/${PARTITION_NAME}_request_size.h/g"
 find $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase} -type f | xargs sed -i "s/routing.h/${PARTITION_NAME}_routing.h/g"
@@ -47,8 +67,8 @@ sed -i "s/${PARTITION_NAME}_asn1SccSint/asn1SccSint/g" $CD_INSTALLATION_DIRECTOR
 sed -i "s/${PARTITION_NAME}_asn1SccSint/asn1SccSint/g" $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}/dataview-uniq.c
 sed -i "/^} ${PARTITION_NAME}_asn1Scc%{ProjectNameUpperCase}_Private_Data;.*/a typedef ${PARTITION_NAME}_asn1Scc%{ProjectNameUpperCase}_Private_Data %{ProjectNameLowerCase}_private_data;" $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}/dataview-uniq.h
 sed -i "s/GENERATED_ASN1SCC/GENERATED_${PARTITION_NAME}_ASN1SCC/g" $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}/dataview-uniq.h
-sed -i "s/put(sender_pid,/put((asn1SccPID)0,/g" $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}/driver_transport.cc
-sed -i '/^#include "dataview-uniq.h".*/a #include <drivers_config.h>' $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}/%{ProjectNameLowerCase}.h
+sed -i '/^#include "asn1crt.h".*/a #include <drivers_config.h>' $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}/dataview-uniq.h
+sed -i "s/put(sender_pid,/put((asn1SccPID)0,/g" $CD_INSTALLATION_DIRECTORY/%{ProjectNameLowerCase}/driver_transport.c*
 
 for name in $FUNCTION_NAMES; do
     FUNCTION_PID=$(echo ${name,,} | sed -e "s/name=/PID_/g" | sed -e "s/\\"//g")
