1798448f3ea94fb2dd59f3dcdf188c12f053142a
Denis Gofman <denis@vikingsoftware.com>
2024-01-31 05:04:26 +0100

    Unit test for topohelp::geom::adjustFromPoint

--- tests/unittests/topohelper/geometry/tst_adjustFromPoint.cpp
+++ tests/unittests/topohelper/geometry/tst_adjustFromPoint.cpp
@@ -30,13 +30,42 @@ class TestAdjustFromPoint : public QObject
 private slots:
     void testAdjustFromPoint_data()
     {
-        QSKIP("Not implemented yet");
+        QTest::addColumn<QPointF>("pos");
+        QTest::addColumn<qreal>("adjustment");
+        QTest::addColumn<QRectF>("expectedResult");
+
+        constexpr QPointF pnt0(0., 0.);
+        constexpr QPointF testPoint(50., 50.);
+        constexpr qreal shift(10.);
+
+        auto expect = [](const QPointF &c, const qreal s) {
+            QRectF r(QPointF(), QSizeF(s, s));
+            r.moveCenter(c);
+            return r;
+        };
+
+        QTest::newRow("PositiveAdjustment") << testPoint << shift << expect(testPoint, shift);
+        QTest::newRow("NegativeAdjustment") << testPoint << -shift << expect(testPoint, -shift);
+        QTest::newRow("NoAdjustment") << testPoint << 0. << expect(testPoint, 0.);
+
+        QTest::newRow("PositiveAdjustmentZero") << pnt0 << shift << expect(pnt0, shift);
+        QTest::newRow("NegativeAdjustmentZero") << pnt0 << -shift << expect(pnt0, -shift);
+        QTest::newRow("NoPosNoAdjustment") << pnt0 << 0. << expect(pnt0, 0.);
     }
 
     void testAdjustFromPoint()
     {
-        QSKIP("Not implemented yet");
-        // const auto &actualResult = adjustFromPoint(replaceMe);
+        QFETCH(QPointF, pos);
+        QFETCH(qreal, adjustment);
+        QFETCH(QRectF, expectedResult);
+
+        const QRectF &actualResult = adjustFromPoint(pos, adjustment);
+        if ((!actualResult.isValid() && !actualResult.isNull())
+                || (actualResult.isEmpty() && !actualResult.topLeft().isNull())) {
+            qWarning() << "Invalid rect:" << actualResult << "TODO: validate input/clean-up result";
+        }
+
+        QCOMPARE(actualResult, expectedResult);
     }
 };
 
