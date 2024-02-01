5810fa7026c84188fb9e70af3fecb36a124e7c9c
Denis Gofman <denis@vikingsoftware.com>
2024-02-02 00:03:24 +0100

    Unit test for topohelp::geom::findSubPath

--- tests/unittests/topohelper/geometry/tst_findSubPath.cpp
+++ tests/unittests/topohelper/geometry/tst_findSubPath.cpp
@@ -17,7 +17,6 @@ along with this program. If not, see <https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "topohelper/geometry.h"
 
-#include <QPointF>
 #include <QtTest>
 
 using namespace topohelp;
@@ -30,13 +29,60 @@ class TestFindSubPath : public QObject
 private slots:
     void testFindSubPath_data()
     {
-        QSKIP("Not implemented yet");
+        QTest::addColumn<QRectF>("itemRect");
+        QTest::addColumn<PointsList>("prevPoints");
+        QTest::addColumn<PointsList>("nextPoints");
+        QTest::addColumn<bool>("strict");
+        QTest::addColumn<QList<PointsList>>("expectedResult");
+
+        QTest::newRow("NoTouchNoStrict") << QRectF(30, 30, 40, 40) << PointsList { QPointF(10, 10), QPointF(20, 20) }
+                                         << PointsList { QPointF(80, 80), QPointF(90, 90) } << false
+                                         << QList<PointsList> {
+                                                { QPointF(10, 10), QPointF(20, 20), QPointF(20, 86), QPointF(86, 86) },
+                                                { QPointF(10, 10), QPointF(20, 20), QPointF(86, 20), QPointF(86, 86) },
+                                                { QPointF(10, 10), QPointF(20, 20), QPointF(20, 14), QPointF(86, 14) },
+                                                { QPointF(10, 10), QPointF(20, 20), QPointF(86, 20), QPointF(86, 14) },
+                                                { QPointF(10, 10), QPointF(20, 20), QPointF(20, 86), QPointF(14, 86) },
+                                                { QPointF(10, 10), QPointF(20, 20), QPointF(14, 20), QPointF(14, 86) },
+                                            };
+
+        QTest::newRow("NoTouchStrict") << QRectF(30, 30, 40, 40) << PointsList { QPointF(10, 10), QPointF(20, 20) }
+                                       << PointsList { QPointF(80, 80), QPointF(90, 90) } << true
+                                       << QList<PointsList> {
+                                              { QPointF(10, 10), QPointF(20, 20), QPointF(20, 86), QPointF(86, 86) },
+                                              { QPointF(10, 10), QPointF(20, 20), QPointF(86, 20), QPointF(86, 86) },
+                                              { QPointF(10, 10), QPointF(20, 20), QPointF(20, 14), QPointF(86, 14) },
+                                              { QPointF(10, 10), QPointF(20, 20), QPointF(86, 20), QPointF(86, 14) },
+                                              { QPointF(10, 10), QPointF(20, 20), QPointF(20, 86), QPointF(14, 86) },
+                                              { QPointF(10, 10), QPointF(20, 20), QPointF(14, 20), QPointF(14, 86) },
+                                          };
+
+        QTest::newRow("TouchNoStrict") << QRectF(30, 30, 40, 40) << PointsList { QPointF(10, 10), QPointF(35, 35) }
+                                       << PointsList { QPointF(65, 65), QPointF(90, 90) } << false
+                                       << QList<PointsList> {
+                                              { QPointF(10, 10), QPointF(35, 35), QPointF(35, 86), QPointF(86, 86) },
+                                              { QPointF(10, 10), QPointF(35, 35), QPointF(86, 35), QPointF(86, 86) },
+                                              { QPointF(10, 10), QPointF(35, 35), QPointF(35, 14), QPointF(86, 14) },
+                                              { QPointF(10, 10), QPointF(35, 35), QPointF(86, 35), QPointF(86, 14) },
+                                              { QPointF(10, 10), QPointF(35, 35), QPointF(35, 86), QPointF(14, 86) },
+                                              { QPointF(10, 10), QPointF(35, 35), QPointF(14, 35), QPointF(14, 86) },
+                                          };
+
+        QTest::newRow("TouchStrict") << QRectF(30, 30, 40, 40) << PointsList { QPointF(10, 10), QPointF(35, 35) }
+                                     << PointsList { QPointF(65, 65), QPointF(90, 90) } << true << QList<PointsList> {};
     }
 
     void testFindSubPath()
     {
-        QSKIP("Not implemented yet");
-        // const auto &actualResult = findSubPath(replaceMe);
+        QFETCH(QRectF, itemRect);
+        QFETCH(PointsList, prevPoints);
+        QFETCH(PointsList, nextPoints);
+        QFETCH(bool, strict);
+        QFETCH(QList<PointsList>, expectedResult);
+
+        const auto &actualResult = findSubPath(itemRect, prevPoints, nextPoints, strict);
+
+        QCOMPARE(actualResult, expectedResult);
     }
 };
 
