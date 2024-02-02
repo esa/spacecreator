069ed95a85095ddfdd307765520df5b0eabd0fee
Denis Gofman <denis@vikingsoftware.com>
2024-02-02 01:02:07 +0100

    Unit test for topohelp::geom::coordinates (rect)

--- tests/unittests/topohelper/geometry/tst_coordinates_rect.cpp
+++ tests/unittests/topohelper/geometry/tst_coordinates_rect.cpp
@@ -30,13 +30,38 @@ class TestCoordinates_rect : public QObject
 private slots:
     void testCoordinates_rect_data()
     {
-        QSKIP("Not implemented yet");
+        QTest::addColumn<QRectF>("rect");
+        QTest::addColumn<QVector<qint32>>("expectedResult");
+
+        constexpr QPointF point100x100(100, 100);
+        QTest::newRow("CeilFloor_1") << QRectF(QPointF(10.5, 20.9), QPointF(30.4, 40.1))
+                                     << QVector<qint32> { 11, 21, 30, 40 };
+        QTest::newRow("CeilFloor_2") << QRectF(QPointF(30.4, 10.5), QPointF(40.1, 20.9))
+                                     << QVector<qint32> { 30, 11, 40, 21 };
+
+        QTest::newRow("EmptyInput") << QRectF {} << QVector<qint32> {};
+        QTest::newRow("Zero") << QRectF(QPointF(0, 0), point100x100) << QVector<qint32> { 0, 0, 100, 100 };
+        QTest::newRow("LargeOK") << QRectF(point100x100, QPointF(1000000, 1000000))
+                                 << QVector<qint32> { 100, 100, 1000000, 1000000 };
+        QTest::newRow("MaxMinOK_1") << QRectF(QPointF(-2147483648, -2147483648), QPointF(2147483647, 2147483647))
+                                    << QVector<qint32> { -2147483648, -2147483648, 2147483647, 2147483647 };
+        QTest::newRow("MaxMinOK_2") << QRectF(QPointF(100, -2147483648), QPointF(2147483647, -100))
+                                    << QVector<qint32> { 100, -2147483648, 2147483647, -100 };
+
+        // negative WxH:
+        QTest::newRow("Large!OK") << QRectF(QPointF(1000000, 1000000), point100x100) << QVector<qint32> {};
+        QTest::newRow("MaxMin!OK_1") << QRectF(QPointF(2147483647, -2147483648), point100x100) << QVector<qint32> {};
+        QTest::newRow("MaxMin!OK_2") << QRectF(point100x100, QPointF(2147483647, -2147483648)) << QVector<qint32> {};
     }
 
     void testCoordinates_rect()
     {
-        QSKIP("Not implemented yet");
-        // const auto &actualResult = coordinates_rect(replaceMe);
+        QFETCH(QRectF, rect);
+        QFETCH(QVector<qint32>, expectedResult);
+
+        const auto &actualResult = coordinates(rect);
+
+        QCOMPARE(actualResult, expectedResult);
     }
 };
 
