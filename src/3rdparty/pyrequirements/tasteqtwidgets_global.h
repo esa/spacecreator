3a65c2b63c025d1b7c52d4af5202c6e37dfbe644
Hugo Jimenez Hernandez <hugo@vikingsoftware.com>
2024-03-18 09:18:40 +0100

    Added reviews widget to python module

--- src/3rdparty/pyrequirements/tasteqtwidgets_global.h
+++ src/3rdparty/pyrequirements/tasteqtwidgets_global.h
@@ -11,5 +11,9 @@
 #include <requirements/requirementsmanager.h>
 #include <requirements/requirementsmodelbase.h>
 #include <requirements/ui/requirementswidget.h>
+#include <reviews/review.h>
+#include <reviews/reviewsmanager.h>
+#include <reviews/reviewsmodelbase.h>
+#include <reviews/reviewswidget.h>
 #include <tracecommon/issuesmanager.h>
 #include <tracecommon/tracecommonlibrary.h>
