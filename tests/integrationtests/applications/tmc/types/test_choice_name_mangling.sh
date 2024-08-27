0f1e0ed887ccdf9116057313acba3acc697b6d3c
Rafał Babski <rbabski@n7space.com>
2024-08-27 23:34:10 +0200

    Added mising file

---
+++ tests/integrationtests/applications/tmc/types/test_choice_name_mangling.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TMC=$SPACECREATOR_BUILD_DIR/bin/tmc
+SPIN=spin
+CC=gcc
+ASN1SCC=asn1scc
+
+# diff ignoring white space and blank lines
+DIFF="diff -w -B"
+TEST_OUTPUT_DIR=output
+RESOURCE_DIR=resources/name-mangling-value-generation
+
+echo "Running TMC test: ${0##*/}"
+
+rm -rf $TEST_OUTPUT_DIR
+mkdir $TEST_OUTPUT_DIR
+
+# Translate
+$TMC -iv $RESOURCE_DIR/interfaceview.xml \
+     -o $TEST_OUTPUT_DIR
+
+$ASN1SCC -uPER -typePrefix asn1Scc -renamePolicy 3 -c -o "${TEST_OUTPUT_DIR}" "${RESOURCE_DIR}/work/dataview/dataview-uniq.asn"
+
+cd $TEST_OUTPUT_DIR \
+    && $SPIN -a system.pml \
+    && $CC -o system.out *.c \
+    && ./system.out -m1000000 -a -n > system.output \
+    && grep -q "errors: 0" system.output \
+    && cd .. \
+    && rm -r $TEST_OUTPUT_DIR
